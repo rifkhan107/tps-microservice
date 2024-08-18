9cca3472dc3c62854c1711d225aa92a78262e8a0
Rifkhan Jm <rifkhan561@gmail.com>
2024-08-18 16:44:34 +0530

    removed dot env

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
 import { AllExceptionsFilter } from './http-exception.filter';
 
 async function bootstrap() {
-  dotenv.config();  // Ensure environment variables are loaded
+  // Create the NestJS application
   const app = await NestFactory.create(AppModule);
 
   // Enable CORS
@@ -13,6 +12,7 @@ async function bootstrap() {
   // Register the custom exception filter globally
   app.useGlobalFilters(new AllExceptionsFilter());
 
+  // Listen on the specified port, defaulting to 3000 if not set
   await app.listen(process.env.PORT || 3000);
   console.log(`Server running on port ${process.env.PORT || 3000}`);
 }
